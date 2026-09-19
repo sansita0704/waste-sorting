fac6431c143cb8b17d383cba2ea04c40dcbc6a77
bhavyatejasvi <bhavyatejasvi@gmail.com>
2026-09-19 16:47:10 +0530

    refactor: remove EcoLedger and ClassificationPanel components along with associated pages

--- src/pages/AnalyticsPage.jsx
+++
@@ -1,9 +0,0 @@
-import EcoLedger from "../components/ledger/EcoLedger";
-
-export default function AnalyticsPage() {
-  return (
-    <div className="mx-auto max-w-2xl">
-      <EcoLedger />
-    </div>
-  );
-}
--- src/pages/LeaderboardPage.jsx
+++
@@ -1,9 +0,0 @@
-import Leaderboard from "../components/leaderboard/Leaderboard";
-
-export default function LeaderboardPage() {
-  return (
-    <div className="mx-auto max-w-2xl">
-      <Leaderboard />
-    </div>
-  );
-}
